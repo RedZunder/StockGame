49691f55e4c12a917d0f14f34bf54ae72c7ecb05
RedZunder <alt47r@gmail.com>
2023-10-03 13:53:01 +0200

    comments

--- Assets/Scenes/Scripts/MoveDot.cs
+++ Assets/Scenes/Scripts/MoveDot.cs
@@ -11,7 +11,7 @@ public class MoveDot : MonoBehaviour
     [SerializeField] TMPro.TextMeshProUGUI textMP;
     [SerializeField] Camera cam;
     GameObject obj;
-    float gainz, aux = 0, value, perct, camAux= 88;
+    float gains, aux = 0, value, perct, camAux= 88;
     Color color;
 
     void Start()
@@ -19,17 +19,19 @@ public class MoveDot : MonoBehaviour
         value = initialValue;
 
     }
+
     void Update()
     {
+        //Randomly increase or decrease the value of stock: 48% chance of decreasing
         if (Random.Range(0, 1f) > 0.48f)
         {
-            gainz = Random.Range(1f, 8);
-            color = Color.green;
+            gains = Random.Range(1f, 8);
+            color = Color.green;        //color for increase in value
         }
         else
         {
-            gainz = -Random.Range(1f, 8);
-            color = Color.red;
+            gains = -Random.Range(1f, 8);
+            color = Color.red;          //color for decrease in value
 
         }
         if (transform.position.x < 160)
@@ -38,9 +40,9 @@ public class MoveDot : MonoBehaviour
 
             if (Mathf.Abs(transform.position.x - aux) >= diff)
             {
-                aux = drawStonk(color, gainz);
-                transform.position += new Vector3(0, gainz);
-                runNumbers(gainz);
+                aux = drawStonk(color, gains);      //we save in "aux" the position of the last drawn rectangle
+                transform.position += new Vector3(0, gains);
+                runNumbers(gains);
             }
             adjustCamera();
         }
@@ -48,6 +50,7 @@ public class MoveDot : MonoBehaviour
 
     }
     
+    //draw a rectangle according to value increase/decrease
     float drawStonk(Color colour, float gains)
     {
         sprite.transform.localScale = new Vector2(diff, gains);
@@ -58,8 +61,10 @@ public class MoveDot : MonoBehaviour
         obj.transform.position = new Vector3(transform.position.x, transform.position.y + gains / 2);
 
         killParent();
-        return transform.position.x;
+        return transform.position.x;        
     }
+
+    //calculate updated stock value
     void runNumbers(float gains)
     {
         value += gains / 10;
@@ -68,14 +73,14 @@ public class MoveDot : MonoBehaviour
         textMP.text += "      "+perct.ToString("F3")+"%";
     }
 
-    void killParent()
+    void killParent()       //self-explanatory
     {
         obj = obj.transform.parent.gameObject;
         obj.transform.DetachChildren();
         Destroy(obj);
     }
 
-    void adjustCamera()
+    void adjustCamera()     //move the camera to fit the entire graph
     {
         if ((int)transform.position.y + 10 > camAux)
         {
